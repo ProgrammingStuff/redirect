cd2ee5721b10da5b00d610ed07fa717b46e57bee
ProgrammingStuff <30649857+ProgrammingStuff@users.noreply.github.com>
2017-09-05 19:28:03 +0300

    Create values.py

---
+++ values.py
@@ -0,0 +1,4 @@
+values = {"token" : "MzQxOTY5MjkxMzEyMTAzNDI0.DGIzPA.9LePktkmtCJGzwK2fMu54_MG7mI", "prefix" : "!"}
+prefix = values["prefix"]
+responses  = {"help" : "Commands:\n¤ " + prefix + "help - Shows this message.\n¤ "
+              + prefix + "status - Shows the status of the Mojang services.\n\nAdd this bot to your own server by pressing this link:\nhttps://discordapp.com/oauth2/authorize?client_id=341969291312103424&scope=bot&permissions=0\n\nIf you discover bugs or problems, please tell me at:\nhttps://docs.google.com/forms/d/e/1FAIpQLScDSlHCsKlLxIujmu8CwkDhWSvQhsOq_TQkwOyrnuLbo3tD6w/viewform?usp=sf_link"}
