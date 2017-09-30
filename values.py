49eb6fd79908635323357279f9d26c41ca8b8006
ProgrammingStuff <30649857+ProgrammingStuff@users.noreply.github.com>
2017-09-30 14:14:01 +0300

    Update values.py

--- values.py
+++ values.py
@@ -1,4 +1,5 @@
 values = {"token" : "MzU0NjY0ODUxMzMyOTIzMzkz.DJBi5Q.8nYIw-GQLhvAeKjY5DFHQI3uVog", "prefix" : "!"}
 prefix = values["prefix"]
 responses  = {"help" : "Commands:\n¤ " + prefix + "help - Shows this message.\n¤ "
-              + prefix + "status - Shows the status of the Mojang services.\n\nAdd this bot to your own server by pressing this link:\nhttps://discordapp.com/oauth2/authorize?client_id=341969291312103424&scope=bot&permissions=0\n\nIf you discover bugs or problems, please tell me at:\nhttps://docs.google.com/forms/d/e/1FAIpQLScDSlHCsKlLxIujmu8CwkDhWSvQhsOq_TQkwOyrnuLbo3tD6w/viewform?usp=sf_link"}
+              + prefix + "status - Shows the status of the Mojang services.\n\nAdd this bot to your own server by pressing this link:\nhttps://discordapp.com/oauth2/authorize?client_id=341969291312103424&scope=bot&permissions=0\n\nIf you discover bugs or problems, please tell me at:\nhttps://docs.google.com/forms/d/e/1FAIpQLScDSlHCsKlLxIujmu8CwkDhWSvQhsOq_TQkwOyrnuLbo3tD6w/viewform?usp=sf_link",
+             "yeet" : "DON'T SAY IT! THIS IS A WARNING! IF YOU GET COUGHT DOING THIS AGAIN YOU WILL GET KICKED!"}
